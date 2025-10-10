dcfefd678f378ee2539b931710620ef1d23c2c85
Ricardo Perre <rperre@Ricardos-MacBook-Air.local>
2025-10-10 21:57:22 +0100

    Relacing ruby_lsp with solargraph

--- lua/custom/plugins/lsp/lsp-config.lua
+++ lua/custom/plugins/lsp/lsp-config.lua
@@ -1,6 +1,8 @@
 return {
     {
         'neovim/nvim-lspconfig',
+        settings = {
+        },
         config = function()
             vim.lsp.config("lua-language-server", {
                 cmd = { 'lua-language-server' },
@@ -20,7 +22,15 @@ return {
                 linters = "rubocop --lsp",
             })
 
-            vim.lsp.enable('ruby_lsp')
+            vim.lsp.config("solargraph", {
+                -- formatter = "rubocop",
+                -- linters = "rubocop --lsp",
+                diagnostics = true,
+                completion = true
+            })
+
+            vim.lsp.enable('solargraph')
+            -- vim.lsp.enable('ruby_lsp')
             vim.lsp.enable('lua-language-server')
         end,
     },
