ba94505d09d8c779bfc118a123dedccdca4fedb2
Ricardo Perre <rperre@Ricardos-MacBook-Air.local>
2025-10-14 05:02:43 +0100

    Fine tuned the behaviour
    The dashboard will open if the user closes all buffers

--- lua/options.lua
+++ lua/options.lua
@@ -7,8 +7,6 @@ vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.mouse = 'a'               -- allow the mouse to be used in nvim
 
 -- [ UI config ]
--- Spacing
--- vim.cmd.colorscheme "gruvbox"
 vim.cmd.colorscheme "NeoCode"
 vim.opt.tabstop = 4      -- number of visual spaces per TAB
 vim.opt.softtabstop = 4  -- number of spaces in tab when editing
@@ -41,11 +39,6 @@ vim.diagnostic.config({
     severity_sort = true,
 })
 
--- local function dashboard_if_only_empty_buffer()
--- end
---
--- mine | au! BufRead | augroup END
-
 local function no_buffers_worth_saving()
     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
         if vim.api.nvim_buf_get_option(bufnr, 'buflisted') and not vim.api.nvim_buf_get_option(bufnr, 'readonly') then -- disregard unlisted buffers
@@ -60,27 +53,40 @@ local function no_buffers_worth_saving()
     end
     return true -- there are no listed, writable, named, nonempty buffers
 end
+local buffer_visible = function(bufnr)
+    for _, tabid in ipairs(vim.api.nvim_list_tabpages()) do
+        for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(tabid)) do
+            local winbufnr = vim.api.nvim_win_get_buf(winid)
+            local winvalid = vim.api.nvim_win_is_valid(winid)
+
+            if winvalid and winbufnr == bufnr then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+
+local function has_content(lines)
+    if #lines > 1 or (#lines == 1 and #lines[1] > 0) then
+        return true
+    end
+    return false
+end
 
 local function open_dashboard_on_empty()
-    -- local Snacks = require("snacks")
-    -- Snacks.dashboard.open()
-    -- vim.notify("BUFF ENTER")
-    -- print(vim.api.nvim_list_bufs())
-    local buf_list = vim.api.nvim_list_bufs()
-    local pr = ""
-    if no_buffers_worth_saving() then
-        vim.notify("Should open dashboard")
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_get_option(bufnr, 'buflisted') and not vim.api.nvim_buf_get_option(bufnr, 'readonly') then -- disregard unlisted buffers
+            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+            if vim.api.nvim_buf_get_name(bufnr) ~= '' and not has_content(lines) then
+                require("snacks").dashboard.open()
+            end
+        end
     end
 end
 
 vim.api.nvim_create_autocmd("BufDelete", {
     group = vim.api.nvim_create_augroup("buf", { clear = true }),
-    -- buffer = args.buf,
     callback = open_dashboard_on_empty,
 })
-
--- vim.api.nvim_create_autocmd("BufEnter", {
---     group = vim.api.nvim_create_augroup("buf", { clear = true }),
---     -- buffer = args.buf,
---     callback = open_dashboard_on_empty,
--- })
