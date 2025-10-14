691481eb114090616f664e2028e03f6a54308ffd
Ricardo Perre <rperre@icapitalnetwork.com>
2025-10-14 08:16:41 +0100

    Plugins and ruby adapter
    Still needs organization and keybindings

--- lua/custom/plugins/lsp/ruby_lsp.lua
+++ lua/custom/plugins/lsp/ruby_lsp.lua
@@ -1,21 +1,33 @@
 return {
-    'adam12/ruby-lsp.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    globals = {
-        "vim"
+    {
+        'adam12/ruby-lsp.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        globals = {
+            "vim"
+        },
+        config = function()
+            -- Auto format when saving
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+                callback = function(args)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = args.buf,
+                        callback = function()
+                            vim.lsp.buf.format { async = false, id = args.data.client_id }
+                        end,
+                    })
+                end
+            })
+        end,
     },
-    config = function()
-        -- Auto format when saving
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-            callback = function(args)
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    buffer = args.buf,
-                    callback = function()
-                        vim.lsp.buf.format { async = false, id = args.data.client_id }
-                    end,
-                })
-            end
-        })
-    end,
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "suketa/nvim-dap-ruby"
+        },
+        config = function()
+            require("dap-ruby").setup()
+        end
+    },
+    { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 }
