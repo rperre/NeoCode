926dcf6826de7a660b8775e8b552482bd2271878
Ricardo Perre <rperre@icapitalnetwork.com>
2025-10-17 20:18:30 +0100

    lint

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,26 +1,27 @@
 return {
-    'nvim-mini/mini.nvim',
-    version = '*',
-    config = function()
-        require('mini.notify').setup()
-        require('mini.icons').setup()
-        require('mini.snippets').setup()
-        require('mini.completion').setup()
-        require('mini.bufremove').setup()
-        require('mini.extra').setup()
-        require('mini.files').setup()
-        require('mini.indentscope').setup()
-        require('mini.git').setup()
-        require('mini.diff').setup()
-        require('mini.statusline').setup()
-        require('mini.tabline').setup()
-        require('mini.comment').setup()
-        require('mini.keymap').setup()
-        require('mini.move').setup()
-        require('mini.pick').setup()
-        require('mini.ai').setup()
+  'nvim-mini/mini.nvim',
+  version = '*',
+  config = function()
+    require('mini.notify').setup()
+    require('mini.icons').setup()
+    require('mini.snippets').setup()
+    require('mini.completion').setup()
+    require('mini.bufremove').setup()
+    require('mini.extra').setup()
+    require('mini.files').setup()
+    require('mini.indentscope').setup()
+    require('mini.git').setup()
+    require('mini.diff').setup()
+    require('mini.statusline').setup()
+    require('mini.tabline').setup()
+    require('mini.comment').setup()
+    require('mini.keymap').setup()
+    require('mini.move').setup()
+    require('mini.pick').setup()
+    require('mini.ai').setup()
+    -- require('mini.hover').setup()
 
-        require('mini.hues').setup({ background = '#000000', foreground = '#cdc4c6' })
-        -- require('mini.colors').get_colorscheme():add_transparency():write({ name = 'NeoCode' })
-    end,
+    -- require('mini.hues').setup({ background = '#000000', foreground = '#cdc4c6' })
+    -- require('mini.colors').get_colorscheme():add_transparency():write({ name = 'NeoCode' })
+  end,
 }
--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,17 +1,17 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts_extend = { "spec" },
-    opts = {
-        preset = "helix",
-        defaults = {},
-    },
-    config = function(_, opts)
-        local wk = require("which-key")
-        wk.setup(opts)
-        if not vim.tbl_isempty(opts.defaults) then
-            LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
-            wk.register(opts.defaults)
-        end
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    preset = "helix",
+    defaults = {},
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    if not vim.tbl_isempty(opts.defaults) then
+      LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+      wk.register(opts.defaults)
     end
+  end
 }
