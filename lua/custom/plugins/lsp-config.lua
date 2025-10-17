ded07b88819c72f1e780b7322cf576f07082000e
Ricardo Perre <rjmperre@gmail.com>
2025-10-17 22:23:37 +0100

    DAP (#15)
    Integration with vim DAP + DAPUI

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -1,59 +1,59 @@
 return {
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "suketa/nvim-dap-ruby"
     },
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "suketa/nvim-dap-ruby"
-        },
-        config = function()
-            require("dap-ruby").setup()
-            require("dapui").setup()
-        end
-    },
-    {
-        'neovim/nvim-lspconfig',
-        config = function()
-            vim.lsp.config("lua-language-server", {
-                cmd = { 'lua-language-server' },
-                filetypes = { 'lua' },
-                root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        }
-                    }
-                }
-            })
+    config = function()
+      require("dap-ruby").setup()
+      require("dapui").setup()
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      vim.lsp.config("lua-language-server", {
+        cmd = { 'lua-language-server' },
+        filetypes = { 'lua' },
+        root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            }
+          }
+        }
+      })
 
-            -- vim.lsp.config("ruby_lsp", {
-            --     formatter = "rubocop",
-            --     linters = "rubocop --lsp",
-            -- })
+      -- vim.lsp.config("ruby_lsp", {
+      --     formatter = "rubocop",
+      --     linters = "rubocop --lsp",
+      -- })
 
-            vim.lsp.config("solargraph", {
-                formatter = "rubocop",
-                linters = "rubocop --lsp",
-                cmd = { 'bundle', 'exec', 'solargraph', 'stdio' },
-                diagnostics = true,
-                completion = true,
-                -- transport = "socket",
-                autoformat = true,
-                definitions = true,
-                folding = true,
-                formatting = true,
-                hover = true,
-                references = true,
-                rename = true,
-                symbols = true,
-            })
+      vim.lsp.config("solargraph", {
+        formatter = "rubocop",
+        linters = "rubocop --lsp",
+        cmd = { 'bundle', 'exec', 'solargraph', 'stdio' },
+        diagnostics = true,
+        completion = true,
+        -- transport = "socket",
+        autoformat = true,
+        definitions = true,
+        folding = true,
+        formatting = true,
+        -- hover = true,
+        references = true,
+        rename = true,
+        symbols = true,
+      })
 
-            vim.lsp.enable('solargraph')
-            -- vim.lsp.enable('ruby_lsp')
-            vim.lsp.enable('lua-language-server')
-        end,
-    },
+      vim.lsp.enable('solargraph')
+      -- vim.lsp.enable('ruby_lsp')
+      vim.lsp.enable('lua-language-server')
+    end,
+  },
 }
--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,40 +1,39 @@
 return {
-
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "olimorris/neotest-rspec",
-    },
-    config = function()
-        require("neotest").setup({
-            adapters = {
-                require("neotest-rspec")
-            },
-        })
-        require("neotest-rspec")({
-            -- Optionally your function can take a position_type which is one of:
-            -- - "file"
-            -- - "test"
-            -- - "dir"
-            rspec_cmd = function(position_type)
-                if position_type == "test" then
-                    return vim.tbl_flatten({
-                        "bundle",
-                        "exec",
-                        "rspec",
-                        "--fail-fast"
-                    })
-                else
-                    return vim.tbl_flatten({
-                        "bundle",
-                        "exec",
-                        "rspec",
-                    })
-                end
-            end
-        })
-    end,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "olimorris/neotest-rspec",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-rspec")
+      },
+    })
+    require("neotest-rspec")({
+      -- Optionally your function can take a position_type which is one of:
+      -- - "file"
+      -- - "test"
+      -- - "dir"
+      rspec_cmd = function(position_type)
+        if position_type == "test" then
+          return vim.tbl_flatten({
+            "bundle",
+            "exec",
+            "rspec",
+            "--fail-fast"
+          })
+        else
+          return vim.tbl_flatten({
+            "bundle",
+            "exec",
+            "rspec",
+          })
+        end
+      end
+    })
+  end,
 }
