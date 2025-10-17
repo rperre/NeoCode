50ea4f5cd33d33d7ef5d29262da4aaa0925451d1
Ricardo Perre <rperre@icapitalnetwork.com>
2025-10-17 20:18:07 +0100

    Some indent line tuning

--- lua/custom/plugins/ident-blankline.lua
+++ lua/custom/plugins/ident-blankline.lua
@@ -1,31 +1,31 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    -- main = "ibl",
-    -- event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require("ibl").setup()
-        -- local blankline = require("ibl")
-        -- local hooks = require("ibl.hooks")
-        --
-        -- vim.opt.list = true
-        -- -- vim.opt.listchars:append("space:⋅")
-        --
-        -- local highlight = {
-        --     "CursorColumn",
-        --     "Whitespace",
-        -- }
-        --
-        -- blankline.setup({
-        --     -- scope = {
-        --     --     enabled = false,
-        --     --     char = "A",
-        --     --     show_exact_scope = true,
-        --     -- },
-        --     indent = { highlight = highlight, char = " " },
-        --     -- whitespace = {
-        --     --     highlight = highlight,
-        --     --     remove_blankline_trail = false,
-        --     -- },
-        -- })
-    end,
+  "lukas-reineke/indent-blankline.nvim",
+  -- main = "ibl",
+  -- event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    -- require("ibl").setup()
+    local ibl = require("ibl")
+    -- local hooks = require("ibl.hooks")
+    --
+    -- vim.opt.list = true
+    -- -- vim.opt.listchars:append("space:⋅")
+    --
+    local highlight = {
+      "CursorColumn",
+      "Whitespace",
+    }
+    --
+    ibl.setup({
+      scope = {
+        enabled = false,
+        char = "A",
+        show_exact_scope = true,
+      },
+      indent = { highlight = highlight, char = " " },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+    })
+  end,
 }
