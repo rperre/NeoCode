45fb18eda3d65781bfea9e598588bf7d2a9ba917
Ricardo Perre <rjmperre@gmail.com>
2025-10-19 23:33:22 +0100

    Weekend updates [10-19-2025] (#25)
    * disable sticy dashboard. this is very buggy, not worth keeping as it is. needs research
    
    * removed event code + gruvbox theme
    
    * organizing keymaps
    
    * Using Telescope cmdline
    
    * no need for fzf commands. we dont use fzf on its own
    
    * mini and snacks conversion

--- lua/keymaps/code.lua
+++ lua/keymaps/code.lua
@@ -1,60 +1,67 @@
 require("which-key").add({
-    {
-        mode = { "n", "v" },
-        { "<leader>c", group = "Code" },
-    },
-    {
-        "<leader>cd",
-        function()
-            vim.diagnostic.open_float()
-        end,
-        desc = "Line diagnostics",
-        mode = "n",
-        noremap = true,
-    },
-    {
-        "<leader>cf",
-        function()
-            vim.lsp.buf.format()
-        end,
-        desc = "Format Buffer",
-        mode = "n",
-        noremap = true,
-    },
-    {
-        "<leader>cD",
-        function()
-            require("dapui").toggle()
-        end,
-        desc = "DAP Toggle",
-        mode = "n",
-        noremap = true,
-    },
-    {
-        "<leader>cR",
-        function()
-            require('dap').continue()
-        end,
-        desc = "DAP Start/Continue",
-        mode = "n",
-        noremap = true,
-    },
-    {
-        "<leader>ct",
-        function()
-            require("neotest").summary.open()
-        end,
-        desc = "Test TUI Open",
-        mode = "n",
-        noremap = true,
-    },
-    {
-        "<leader>cT",
-        function()
-            require("neotest").summary.close()
-        end,
-        desc = "Test TUI Close",
-        mode = "n",
-        noremap = true,
-    },
+  {
+    mode = { "n", "v" },
+    { "<leader>c", group = "Code" },
+  },
+  {
+    "<leader>cl",
+    "<cmd>LazyGit<CR>",
+    desc = "LazyGit",
+    mode = "n",
+    noremap = true,
+  },
+  {
+    "<leader>cd",
+    function()
+      vim.diagnostic.open_float()
+    end,
+    desc = "Line diagnostics",
+    mode = "n",
+    noremap = true,
+  },
+  {
+    "<leader>cf",
+    function()
+      vim.lsp.buf.format()
+    end,
+    desc = "Format Buffer",
+    mode = "n",
+    noremap = true,
+  },
+  {
+    "<leader>cD",
+    function()
+      require("dapui").toggle()
+    end,
+    desc = "DAP Toggle",
+    mode = "n",
+    noremap = true,
+  },
+  {
+    "<leader>cR",
+    function()
+      require('dap').continue()
+    end,
+    desc = "DAP Start/Continue",
+    mode = "n",
+    noremap = true,
+  },
+  {
+    "<leader>ct",
+    function()
+      require("neotest").summary.open()
+    end,
+    desc = "Test TUI Open",
+    mode = "n",
+    noremap = true,
+  },
+  {
+    "<leader>cT",
+    function()
+      require("neotest").summary.close()
+    end,
+    desc = "Test TUI Close",
+    mode = "n",
+    noremap = true,
+  },
 })
