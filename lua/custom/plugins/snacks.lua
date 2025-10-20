67bb6633626e3d87f4eaf7e76625b1c8aa2f7c85
Ricardo Perre <rperre@icapitalnetwork.com>
2025-10-20 16:54:06 +0100

    Better notify behaviour
    All notifies are done thru snacks. Spinner when LSP is scanning. More
    default messages go directly to snacks notify

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -11,22 +11,55 @@ return {
         end,
       },
       sections = {
-        { title = "Recent",         padding = 1 },
-        { section = "recent_files", padding = 1 },
-        { title = "Sessions",       padding = 1 },
-        { section = "projects",     padding = 1 },
-        { section = "keys",         padding = 1 },
+        -- { title = "Recent",         padding = 1 },
+        -- { section = "recent_files", padding = 1 },
+        -- { title = "Sessions",       padding = 1 },
+        { title = "Projects",   padding = 1 },
+        { section = "projects", padding = 1 },
+        -- { section = "keys",     padding = 1 },
       },
     },
-    -- explorer = { enabled = true },
+    explorer = { enabled = true },
     -- indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
-    notifier = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000, -- default timeout in ms
+      width = { min = 40, max = 0.4 },
+      height = { min = 1, max = 0.6 },
+      -- editor margin to keep free. tabline and statusline are taken into account automatically
+      margin = { top = 0, right = 1, bottom = 0 },
+      padding = true,              -- add 1 cell of left/right padding to the notification window
+      sort = { "level", "added" }, -- sort by level and time
+      -- minimum log level to display. TRACE is the lowest
+      -- all notifications are stored in history
+      level = vim.log.levels.TRACE,
+      icons = {
+        error = " ",
+        warn = " ",
+        info = " ",
+        debug = " ",
+        trace = " ",
+      },
+      keep = function(notif)
+        return vim.fn.getcmdpos() > 0
+      end,
+      ---@type snacks.notifier.style
+      style = "fancy",
+      top_down = true,    -- place notifications from top to bottom
+      date_format = "%R", -- time format for notifications
+      -- format for footer when more lines are available
+      -- `%d` is replaced with the number of lines.
+      -- only works for styles with a border
+      ---@type string|boolean
+      more_format = " ↓ %d lines ",
+      refresh = 50, -- refresh at most every 50ms
+    },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
-    words = { enabled = true },
+    -- words = { enabled = true },
   },
 }
